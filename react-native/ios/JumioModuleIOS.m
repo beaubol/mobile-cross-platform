434620843cc497b20886e2761d22c09b407725d9
Lukas Koblmüller <lukas.koblmueller@jumio.com>
2017-04-20 14:00:26 +0200

    Changes react-native ios module.

--- react-native/ios/JumioModuleIOS.m
+++ react-native/ios/JumioModuleIOS.m
@@ -29,142 +29,160 @@ RCT_EXPORT_MODULE();
 
 - (NSArray<NSString *> *)supportedEvents
 {
-  return @[@"EventDocumentData", @"EventCardInfo"];
+    return @[@"EventDocumentData", @"EventCardInfo"];
 }
 
+#pragma mark - Netverify
+
 RCT_EXPORT_METHOD(initNetverify:(NSString *)apiToken apiSecret:(NSString *)apiSecret dataCenter:(NSString *)dataCenter) {
-  _apiTokenNV = apiToken;
-  _apiSecretNV = apiSecret;
-  NSString *dataCenterLowercase = [dataCenter lowercaseString];
-  _dataCenterNV = ([dataCenterLowercase isEqualToString:@"eu"]) ? JumioDataCenterEU : JumioDataCenterUS;
-  
-  [self initNetverifySDK];
+    _apiTokenNV = apiToken;
+    _apiSecretNV = apiSecret;
+    NSString *dataCenterLowercase = [dataCenter lowercaseString];
+    _dataCenterNV = ([dataCenterLowercase isEqualToString: @"eu"]) ? JumioDataCenterEU : JumioDataCenterUS;
+    
+    [self initNetverifySDK];
 }
 
 RCT_EXPORT_METHOD(startNetverify) {
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController presentViewController:self.netverifyViewController animated:YES completion:nil];
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController presentViewController: _netverifyViewController animated:YES completion: nil];
 }
 
+#pragma mark - BAM Checkout
+
 RCT_EXPORT_METHOD(initBAM:(NSString *)apiToken apiSecret:(NSString *)apiSecret dataCenter:(NSString *)dataCenter) {
-  _apiTokenBAM = apiToken;
-  _apiSecretBAM = apiSecret;
-  NSString *dataCenterLowercase = [dataCenter lowercaseString];
-  _dataCenterBAM = ([dataCenterLowercase isEqualToString:@"eu"]) ? JumioDataCenterEU : JumioDataCenterUS;
-  
-  [self initBAMSDK];
+    _apiTokenBAM = apiToken;
+    _apiSecretBAM = apiSecret;
+    NSString *dataCenterLowercase = [dataCenter lowercaseString];
+    _dataCenterBAM = ([dataCenterLowercase isEqualToString: @"eu"]) ? JumioDataCenterEU : JumioDataCenterUS;
+    
+    [self initBAMSDK];
 }
 
 RCT_EXPORT_METHOD(startBAM) {
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController presentViewController:self.bamViewController animated:YES completion:nil];
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController presentViewController: _bamViewController animated: YES completion: nil];
 }
 
 #pragma mark - JumioInit
 
 - (void)initNetverifySDK {
-  NetverifyConfiguration *configuration = [NetverifyConfiguration new];
-  configuration.delegate = self;
-  configuration.merchantApiToken = _apiTokenNV;
-  configuration.merchantApiSecret = _apiSecretNV;
-  configuration.dataCenter = _dataCenterNV;
-  
-  //******* optional customization *******
-  configuration.preselectedDocumentTypes = NetverifyDocumentTypeAll;
-  configuration.requireVerification = NO;
-  
-  self.netverifyViewController = [[NetverifyViewController alloc] initWithConfiguration:configuration];
+    NetverifyConfiguration *configuration = [NetverifyConfiguration new];
+    configuration.delegate = self;
+    configuration.merchantApiToken = _apiTokenNV;
+    configuration.merchantApiSecret = _apiSecretNV;
+    configuration.dataCenter = _dataCenterNV;
+    
+    //******* optional customization *******
+    configuration.preselectedDocumentTypes = NetverifyDocumentTypeAll;
+    configuration.requireVerification = NO;
+    
+    _netverifyViewController = [[NetverifyViewController alloc] initWithConfiguration: configuration];
 }
 
 - (void)initBAMSDK {
-  NetswipeConfiguration *configuration = [NetswipeConfiguration new];
-  configuration.delegate = self;
-  configuration.merchantApiToken = _apiTokenBAM;
-  configuration.merchantApiSecret = _apiSecretBAM;
-  configuration.dataCenter = _dataCenterBAM;
-  
-  //******* optional customization *******
-  configuration.expiryRequired = YES;
-  configuration.cvvRequired = YES;
-  configuration.cardHolderNameRequired = YES;
-  configuration.cardHolderNameEditable = NO;
-  configuration.sortCodeAndAccountNumberRequired = NO;
-  configuration.cardNumberMaskingEnabled = NO;
-  configuration.vibrationEffectEnabled = NO;
-  
-  self.bamViewController = [[NetswipeViewController alloc]initWithConfiguration:configuration];
+    NetswipeConfiguration *configuration = [NetswipeConfiguration new];
+    configuration.delegate = self;
+    configuration.merchantApiToken = _apiTokenBAM;
+    configuration.merchantApiSecret = _apiSecretBAM;
+    configuration.dataCenter = _dataCenterBAM;
+    
+    //******* optional customization *******
+    configuration.expiryRequired = YES;
+    configuration.cvvRequired = YES;
+    configuration.cardHolderNameRequired = YES;
+    configuration.cardHolderNameEditable = NO;
+    configuration.sortCodeAndAccountNumberRequired = NO;
+    configuration.cardNumberMaskingEnabled = NO;
+    configuration.vibrationEffectEnabled = NO;
+    
+    _bamViewController = [[NetswipeViewController alloc]initWithConfiguration: configuration];
 }
 
 #pragma mark - NetverifySDKDelegate
 
 - (void) netverifyViewController:(NetverifyViewController *)netverifyViewController didFinishWithDocumentData:(NetverifyDocumentData *)documentData scanReference:(NSString *)scanReference {
-  // Successfully retrieved document data
-  NSLog(@"NetverifySDK finished successfully with scan reference: %@", scanReference);
-  
-  // CUSTOMIZE YOUR CODE HERE
+    NSLog(@"NetverifySDK finished successfully with scan reference: %@", scanReference);
     
-  // dismiss and send document data (and card info) back to react native
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController dismissViewControllerAnimated:YES completion: ^{
-    [self sendEventWithName:@"EventDocumentData" body:@{@"firstName": documentData.firstName, @"lastName": documentData.lastName}];
-  }];
+    // dismiss and send document data back to react native
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController dismissViewControllerAnimated: YES completion: ^{
+        [self sendEventWithName: @"EventDocumentData" body: @{
+                                                              @"firstName": documentData.firstName,
+                                                              @"lastName": documentData.lastName
+                                                              // ...
+                                                              }];
+    }];
 }
 
 - (void) netverifyViewController:(NetverifyViewController *)netverifyViewController didCancelWithError:(NSError *)error scanReference:(NSString *)scanReference {
-  NSLog(@"NetverifySDK cancelled with error: %@, scanReference: %@", error.localizedDescription, scanReference);
-  
-  // HANDLE ERRORS HERE
+    NSLog(@"NetverifySDK cancelled with error: %@, scanReference: %@", error.localizedDescription, scanReference);
     
-  // dismiss
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+    // send error event
+    [self sendError: error];
+    
+    // dismiss
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void) netverifyViewController:(NetverifyViewController *)netverifyViewController didFinishInitializingWithError:(NSError *)error {
-  if (error != nil) {
-    NSLog(@"NetverifySDK initialized with error: %@", error.localizedDescription);
-    // HANDLE THE ERROR HERE
-  }
+    if (error != nil) {
+        NSLog(@"NetverifySDK initialized with error: %@", error.localizedDescription);
+        
+        // send error event
+        [self sendError: error];
+    }
 }
-
 
 #pragma mark - BAMSDKDelegate
 
 - (void) netswipeViewController:(NetswipeViewController *)controller didFinishScanWithCardInformation:(NetswipeCardInformation *)cardInformation scanReference:(NSString *)scanReference {
-  // Successfully retrieved card information
-  NSLog(@"BAMSDK finished successfully with scan reference: %@", scanReference);
-  
-  // CUSTOMIZE YOUR CODE HERE
+    NSLog(@"BAMSDK finished successfully with scan reference: %@", scanReference);
     
-  // forward to netverify or send data back to react native
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController dismissViewControllerAnimated:YES completion: ^(void) {
-      [self sendEventWithName:@"EventCardInfo" body:@{@"cardNumberGrouped": cardInformation.cardNumberGrouped}];
-  }];
+    // send data back to react native
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController dismissViewControllerAnimated:YES completion: ^(void) {
+        [self sendEventWithName:@"EventCardInfo" body:@{
+                                                        @"cardNumberGrouped": cardInformation.cardNumberGrouped,
+                                                        @"cardHolderName": cardInformation.cardHolderName
+                                                        // ...
+                                                        }];
+    }];
 }
 
 - (void)netswipeViewController:(NetswipeViewController *)controller didCancelWithError:(NSError *)error{
-  NSInteger code = error.code;
-  NSString *message = error.localizedDescription;
-  NSLog(@"Canceled with error code: %ld, message: %@", (long)code, message);
+    NSInteger code = error.code;
+    NSString *message = error.localizedDescription;
+    NSLog(@"Canceled with error code: %ld, message: %@", (long)code, message);
     
-  // HANDLE THE ERROR HERE
+    // send error event
+    [self sendError: error];
     
-  // dismiss
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+    // dismiss
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController dismissViewControllerAnimated: YES completion: nil];
 }
 
 - (void)netswipeViewController:(NetswipeViewController *)controller didStartScanAttemptWithRequestReference:(NSString *)requestReference{
-  NSLog(@"NetswipeViewController did start scan attempt with request reference: %@", requestReference);
+    NSLog(@"NetswipeViewController did start scan attempt with request reference: %@", requestReference);
 }
 
 - (void)netswipeViewController:(NetswipeViewController *)controller didCancelWithError:(NSError *)error scanReference:(NSString *)scanReference {
-  NSLog(@"BAMSDK cancelled with error: %@, scanReference: %@", error.localizedDescription, scanReference);
-  
-  // HANDLE THE ERROR HERE
-  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-  [delegate.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"BAMSDK cancelled with error: %@, scanReference: %@", error.localizedDescription, scanReference);
+    
+    // send error event
+    [self sendError: error];
+    
+    // dismiss
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [delegate.window.rootViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+# pragma mark - Helper Methods
+
+- (void) sendError:(NSError *)error {
+    // TODO:...
 }
 
 @end
