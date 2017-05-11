faaef6b847f953cd5000169dcc9e72053fb892af
Lukas Koblmüller <lukas.koblmueller@jumio.com>
2017-05-11 11:24:49 +0200

    Updated react-native android module.

--- react-native/android/MainActivity.java
+++ react-native/android/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.bridge.Arguments;
@@ -23,8 +24,6 @@ import com.jumio.nv.NetverifySDK;
 
 // add JumioModule
 // import com.<YOURPACKAGE>.JumioModule;
-
-import java.util.ArrayList;
 
 public class MainActivity extends ReactActivity {
 
